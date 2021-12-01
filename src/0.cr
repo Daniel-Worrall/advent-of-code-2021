fde12c96dd7b6da00152cdb9b3fad3d763343493
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2021-12-01 23:26:04 +0000

    Add day 0

---
+++ src/0.cr
@@ -0,0 +1,14 @@
+require "./input"
+
+module AoC
+  extend self
+
+  def zero
+  end
+
+  def zero_b
+  end
+end
+
+puts AoC.zero
+puts AoC.zero_b
---
+++ src/input.cr
@@ -0,0 +1,5 @@
+module AoC
+  def self.input(day)
+    File.read("#{__DIR__}/../input/#{day}.txt")
+  end
+end
